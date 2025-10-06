5f4e302ccbfa10d60f7ae2ae1be8075d318b33b8
a.amerkanova <assel.amerkanova@digitaleconomy.kz>
2025-10-06 14:29:07 +0500

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,27 @@
 import React from 'react';
 
 function Footer() {
-  return <footer>© 2025 My Book Store</footer>;
+  return <footer>
+    
+<div className="flex"> 
+    <div>
+
+<img src="/src/assets/footer-books.svg" />
+    </div>
+
+    <div>
+<h1> Your favourite Reads
+Are Here! </h1>
+
+    </div>
+    </div>
+    
+    © 2025 My Book Store
+    
+    <div className="bg-red text-white p-4">Tailwind Works!</div>
+
+    
+    </footer>;
 }
 
 export default Footer;
