6a8762648ec2afa682726e586a5aac6ec5412bfc
a.amerkanova <assel.amerkanova@digitaleconomy.kz>
2025-10-06 15:00:44 +0500

    style

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,27 +1,26 @@
-import React from 'react';
+import React from "react";
+import footerBooks from "../assets/footer-books.svg";
 
 function Footer() {
-  return <footer>
-    
-<div className="flex"> 
-    <div>
+  return (
+    <footer>
+      <div className="flex w-full p-10 gap-[200px]">
+        <div>
+          <img src={footerBooks} alt="Books" />
+        </div>
 
-<img src="/src/assets/footer-books.svg" />
-    </div>
-
-    <div>
-<h1> Your favourite Reads
-Are Here! </h1>
-
-    </div>
-    </div>
-    
-    © 2025 My Book Store
-    
-    <div className="bg-red text-white p-4">Tailwind Works!</div>
-
-  
-    </footer>;
+        <div className="w-[40%] flex justify-center align-center">
+          <div className="pt-10"> 
+          <h1 className="text-[40px]"> Your favourite <br/>   <span className="text-[#CC9600]">Reads Are Here!</span>  </h1>
+       
+          <p>Buy your favorite books online with ease! Enjoy exclusive offers and discounts on selected titles. Dive into our collection and find special deals that make reading more affordable. Shop now and unlock more savings with every purchase!</p>
+        </div>
+        </div>
+      </div>
+      © 2025 My Book Store
+      <div className="bg-red text-white p-4">Tailwind Works!</div>
+    </footer>
+  );
 }
 
 export default Footer;
