2615f90169cfccfd5cec757bf2a48fdf9eeb08c3
a.amerkanova <assel.amerkanova@digitaleconomy.kz>
2025-12-02 14:09:19 +0500

    function

--- src/pages/Clean/Clean.js
+++ src/pages/Clean/Clean.js
@@ -1,12 +1,43 @@
+import React from "react";
 import szpt from "../../data/szpt.json";
 
 function Clean() {
+  function extractNamesDeep(data) {
+    const result = [];
 
-      return ( 
-        <>
-        <h4>Clean</h4>
-        </>
-       );
+    function search(obj) {
+      if (!obj || typeof obj !== "object") return;
+
+      if (obj.name?.ru) {
+        result.push(obj.name.ru);
+      }
+
+      for (const key in obj) {
+        search(obj[key]);
+      }
+    }
+
+    data.forEach(search);
+    return result;
+  }
+
+  const names = extractNamesDeep(szpt);
+
+  return (
+    <div style={{ padding: "20px" }}>
+      <h3>Найденные name.ru: ({names.length})</h3>
+
+      {names.length === 0 ? (
+        <p>Нет данных</p>
+      ) : (
+        <ul>
+          {names.map((n, index) => (
+            <li key={index}>{n}</li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
 }
 
 export default Clean;
